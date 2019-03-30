ffeca45a1f4752120e63318ad671a663921bc276
Quincey James <qjames2@uwo.ca>
2019-03-30 12:41:03 -0400

    Reformatting

--- src/states/subscriber/AbstractState.java
+++ src/states/subscriber/AbstractState.java
@@ -5,7 +5,8 @@ package states.subscriber;
 public abstract class AbstractState implements IState {
 	
 	public AbstractState()  {
-		
+		// TODO: delete IState
+		// TODO: uses each state in the demo to show how each state implements it
 	}
 	
 }
--- src/strategies/publisher/AbstractStrategy.java
+++ src/strategies/publisher/AbstractStrategy.java
@@ -3,7 +3,7 @@ package strategies.publisher;
 public abstract class AbstractStrategy implements IStrategy{
 	
 	public AbstractStrategy() {
-		
+		// TODO: delete the IStrategy 
 	}
 	
 }
