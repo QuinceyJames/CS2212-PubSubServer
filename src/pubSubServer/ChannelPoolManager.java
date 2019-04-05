6b29f4852446b47004bad77a51960e40b5923cc4
Quincey James <qjames2@uwo.ca>
2019-04-05 17:29:16 -0400

    fixed #listChannels

--- src/pubSubServer/ChannelPoolManager.java
+++ src/pubSubServer/ChannelPoolManager.java
@@ -4,31 +4,31 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-
 /**
  * @author kkontog, ktsiouni, mgrigori
- *  
- *  implements the Singleton Design Pattern
- *  
- *  holds the collection of AbstractChannel type entities and provides the methods for manipulating thes collections
+ * 
+ *         implements the Singleton Design Pattern
+ * 
+ *         holds the collection of AbstractChannel type entities and provides
+ *         the methods for manipulating thes collections
  */
 public class ChannelPoolManager {
 
 	private static ChannelPoolManager instance = null;
 	private Map<String, AbstractChannel> channelsMap = new HashMap<String, AbstractChannel>();
-	
 
 	private ChannelPoolManager() {
 		try {
-		BufferedReader channelListReader = new BufferedReader(new FileReader(new File("Channels.chl")));
-		while(channelListReader.ready())
-			addChannel(channelListReader.readLine());
-		channelListReader.close();
-		}catch(IOException ioe) {
+			BufferedReader channelListReader = new BufferedReader(new FileReader(new File("Channels.chl")));
+			while (channelListReader.ready())
+				addChannel(channelListReader.readLine());
+			channelListReader.close();
+		} catch (IOException ioe) {
 			System.out.println("Error with loading from file, creating one no_theme_channel");
 			addChannel("no_theme");
 		}
@@ -41,9 +41,10 @@ public class ChannelPoolManager {
 		return instance;
 	}
 
-	
 	/**
-	 * creates a new AbstractChannel, adds it to the collections and returns it to the caller
+	 * creates a new AbstractChannel, adds it to the collections and returns it to
+	 * the caller
+	 * 
 	 * @param channelName the name of the new AbstractChannel
 	 * @return the new AbstractChannel
 	 */
@@ -54,9 +55,9 @@ public class ChannelPoolManager {
 		return ch;
 	}
 
-
 	/**
 	 * removes an AbstractChannel from the collections of available AbstractChannels
+	 * 
 	 * @param channelName the name of the AbstractChannel to be removed
 	 */
 	protected void deleteChannel(String channelName) {
@@ -64,18 +65,18 @@ public class ChannelPoolManager {
 		channelsMap.remove(channelName);
 	}
 
-	
 	/**
 	 * can be used to list all the Channels available in the PubSubServer
+	 * 
 	 * @return a list of type {@link AbstractChannel}
 	 */
 	protected List<AbstractChannel> listChannels() {
-		return List.copyOf(channelsMap.values());
+		return new ArrayList<>(channelsMap.values());
 	}
 
-	
 	/**
 	 * returns the object of AbstractChannel using a name as lookup information
+	 * 
 	 * @param channelName the name of the AbstractChannel to be returned
 	 * @return the appropriate instance of an AbstractChannel subclass
 	 */
@@ -83,13 +84,13 @@ public class ChannelPoolManager {
 		return channelsMap.get(channelName);
 	}
 
-	
 	/**
 	 * accessor for the ChannelMap use with great caution
+	 * 
 	 * @return a Map from String to {@link AbstractChannel}
 	 */
 	protected Map<String, AbstractChannel> getChannelsMap() {
 		return channelsMap;
 	}
-	
+
 }
