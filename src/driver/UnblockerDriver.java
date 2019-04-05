b18174fa1a5e271f13e542523718493e8192997a
Zhilong Zhang <zzhan746>
2019-04-05 16:16:56 -0400

    Unblock driver method

--- src/driver/UnblockerDriver.java
+++ src/driver/UnblockerDriver.java
@@ -1,5 +1,18 @@
 package driver;
 
-public class UnblockerDriver {
+import java.util.StringTokenizer;
 
+import orchestration.Orchestration;
+import publishers.AbstractPublisher;
+import subscribers.AbstractSubscriber;
+import pubSubServer.ChannelAccessControl;
+import pubSubServer.SubscriptionManager;
+
+public class UnblockerDriver {
+	public void execute(StringTokenizer tokenizer) {
+		AbstractSubscriber subscriber = Orchestration.getInstance().getSubscriber(Integer.parseInt(tokenizer.nextToken()));
+		String channelName = tokenizer.nextToken();
+		SubscriptionManager.getInstance().Unblock(subscriber, channelName);
+
+}
 }
