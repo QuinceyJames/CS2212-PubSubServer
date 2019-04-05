5a0f5429d9df4d6b08ffb1e5b4fb3ea911ebc1a1
Quincey James <qjames2@uwo.ca>
2019-04-05 18:53:17 -0400

    changed from lazy to static instantiation

--- src/publishers/PublisherDiscovery.java
+++ src/publishers/PublisherDiscovery.java
@@ -2,35 +2,34 @@ package publishers;
 
 import subscribers.AbstractSubscriber;
 
-
 /**
- * @author kkontog, ktsiouni, mgrigori
- * MUST IMPLEMENT the Singleton design pattern
- * Allows for the discovery of available channels for subscription from {@link AbstractSubscriber} that want to subscribe to them
+ * @author kkontog, ktsiouni, mgrigori MUST IMPLEMENT the Singleton design
+ *         pattern Allows for the discovery of available channels for
+ *         subscription from {@link AbstractSubscriber} that want to subscribe
+ *         to them
  */
 public class PublisherDiscovery {
-	
-	private static PublisherDiscovery instance = null;
-	
-	private PublisherDiscovery() {}
-	
-	public static PublisherDiscovery getInstance() {
-		if (instance == null)
-			instance = new PublisherDiscovery();
-		return instance;
+
+	private static PublisherDiscovery INSTANCE = new PublisherDiscovery();
+
+	private PublisherDiscovery() {
 	}
-	
+
+	public static PublisherDiscovery getInstance() {
+		return INSTANCE;
+	}
+
 	/**
 	 * @param subscriberID the ID number of the looked-up subscriber
-	 * @return a {@link AbstractSubscriber} type object corresponding the the ID provided as input
+	 * @return a {@link AbstractSubscriber} type object corresponding the the ID
+	 *         provided as input
 	 */
-	public AbstractPublisher findPublisher(int publisherID) { //may not be able to use ID
-		return PublisherPoolManager.getInstance().findPublisher(publisherID); //may not be able to use ID
+	public AbstractPublisher findPublisher(int publisherID) { // may not be able to use ID
+		return PublisherPoolManager.getInstance().findPublisher(publisherID); // may not be able to use ID
 	}
-	
-	public int findID(AbstractPublisher publisher) { //may not be able to use ID
-		return PublisherPoolManager.getInstance().findID(publisher); //may not be able to use ID
-	}
-	
-}
 
+	public int findID(AbstractPublisher publisher) { // may not be able to use ID
+		return PublisherPoolManager.getInstance().findID(publisher); // may not be able to use ID
+	}
+
+}
--- src/subscribers/SubscriberDiscovery.java
+++ src/subscribers/SubscriberDiscovery.java
@@ -1,32 +1,33 @@
 package subscribers;
 
 /**
- * @author kkontog, ktsiouni, mgrigori
- * MUST IMPLEMENT the Singleton design pattern
- * Allows for the discovery of available channels for subscription from {@link AbstractSubscriber} that want to subscribe to them
+ * @author kkontog, ktsiouni, mgrigori MUST IMPLEMENT the Singleton design
+ *         pattern Allows for the discovery of available channels for
+ *         subscription from {@link AbstractSubscriber} that want to subscribe
+ *         to them
  */
 public class SubscriberDiscovery {
-	
-	private static SubscriberDiscovery instance = null;
-	
-	private SubscriberDiscovery() {}
-	
-	public static SubscriberDiscovery getInstance() {
-		if (instance == null)
-			instance = new SubscriberDiscovery();
-		return instance;
+
+	private static SubscriberDiscovery INSTANCE = new SubscriberDiscovery();
+
+	private SubscriberDiscovery() {
 	}
-	
+
+	public static SubscriberDiscovery getInstance() {
+		return INSTANCE;
+	}
+
 	/**
 	 * @param subscriberID the ID number of the looked-up subscriber
-	 * @return a {@link AbstractSubscriber} type object corresponding the the ID provided as input
+	 * @return a {@link AbstractSubscriber} type object corresponding the the ID
+	 *         provided as input
 	 */
-	public AbstractSubscriber findSubscriber(int subscriberID) { //may not be able to use ID
-		return SubscriberPoolManager.getInstance().findSubscriber(subscriberID); //may not be able to use ID
+	public AbstractSubscriber findSubscriber(int subscriberID) { // may not be able to use ID
+		return SubscriberPoolManager.getInstance().findSubscriber(subscriberID); // may not be able to use ID
 	}
-	
-	public int findID(AbstractSubscriber subscriber) { //may not be able to use ID
-		return SubscriberPoolManager.getInstance().findID(subscriber); //may not be able to use ID
+
+	public int findID(AbstractSubscriber subscriber) { // may not be able to use ID
+		return SubscriberPoolManager.getInstance().findID(subscriber); // may not be able to use ID
 	}
-	
+
 }
