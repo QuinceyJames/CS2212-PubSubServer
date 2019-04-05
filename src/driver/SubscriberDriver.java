32b758231f73b5c321e0d84056ca4f2ac3fdde96
Owner <Owner@172.30.71.133>
2019-04-05 16:36:37 -0400

    Pre conditions added for subscriber and unsubscribe

--- src/driver/SubscriberDriver.java
+++ src/driver/SubscriberDriver.java
@@ -1,22 +1,27 @@
 package driver;
 
 import java.util.StringTokenizer;
-
 import events.AbstractEvent;
 import events.EventFactory;
 import events.EventType;
 import orchestration.Orchestration;
+import pubSubServer.AbstractChannel;
 import pubSubServer.ChannelCreator;
 import pubSubServer.SubscriptionManager;
 import subscribers.AbstractSubscriber;
+import pubSubServer.ChannelDiscovery;
 
 public class SubscriberDriver implements DriverInterface {
 	
 	@Override
 	public void execute(StringTokenizer tokenizer) {
+		
 		AbstractSubscriber subscriber = Orchestration.getInstance().getSubscriber(Integer.parseInt(tokenizer.nextToken()));
-			
-			String channelName = tokenizer.nextToken();
+		String channelName = tokenizer.nextToken();
+		AbstractChannel channel = ChannelDiscovery.getInstance().findChannel(channelName);
+		
+		if (channel != null) {
 			SubscriptionManager.getInstance().subscribe(channelName, subscriber);
+		}		
 	}
 }
