aab2012c7be69ee647562d43281ef600230b4707
talon <talon@172.30.0.104>
2019-04-05 21:20:24 -0400

    added a default publisher test case in the driver file

--- src/driver/PublisherDriver.java
+++ src/driver/PublisherDriver.java
@@ -24,7 +24,6 @@ public class PublisherDriver implements DriverInterface {
 		
 		else {
 			publisher.publish();
-			System.out.println(String.format("%s publishes ******************************", publisher));
 		}
 	}
 
