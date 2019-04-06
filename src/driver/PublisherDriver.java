a9eea77a10fbed0954cf520f8a3bd443a6e607bc
talon <talon@172.30.0.104>
2019-04-05 20:22:04 -0400

    updated PublisherDriver to use scanner

--- src/driver/PublisherDriver.java
+++ src/driver/PublisherDriver.java
@@ -3,6 +3,9 @@ package driver;
 import java.util.Scanner;
 import java.util.StringTokenizer;
 
+import events.AbstractEvent;
+import events.EventFactory;
+import events.EventType;
 import publishers .AbstractPublisher;
 import publishers.PublisherDiscovery;
 
@@ -11,18 +14,19 @@ public class PublisherDriver implements DriverInterface {
 	@Override
 	public void execute(Scanner scanner) {
 		AbstractPublisher publisher = PublisherDiscovery.getInstance().findPublisher(scanner.nextInt());
-//		if (scanner.countTokens() > 2) {
-//			int eventType = Integer.parseInt(tokenizer.nextToken());
-//			EventType type = EventType.values()[eventType];
-//			AbstractEvent newEvent = EventFactory.createEvent(type, publisher, tokenizer.nextToken("|"), tokenizer.nextToken("|"));
-//			publisher.publish(newEvent);
-//		}
-//		
-//		else {
-			publisher.publish();
-//		}
+		if (scanner.hasNext())	{
+			int eventType = scanner.nextInt();
+			EventType type = EventType.values()[eventType];
+			AbstractEvent newEvent = EventFactory.createEvent(type, publisher, scanner.next("|"), scanner.next("|"));
+			publisher.publish(newEvent);
+		}
 		
+		else {
+			publisher.publish();
+		}
 	}
+		
+	
 	public static void main (String[] args) {
 		String test = "1 2 This is a header | This is a payload |";
 		StringTokenizer tokenizer = new StringTokenizer(test);
